25a4c107cdbf63f6a760b918dc4fdcabc5dec043
Alondra Prado <alondra.prado@apptiva.ch>
2026-04-30 10:41:16 +0200

    wrote TODOs

--- tanzverbot_diet.ts
+++ tanzverbot_diet.ts
@@ -3,6 +3,7 @@ export enum Sex {
   Female = "f",
 }
 
+// TODO: Die drei Listen unten zu einer Liste mit Objekten machen
 const foodNames: string[] = [
   "Kellogg's Tresor",
   "Weihenstephan Haltbare Milch",
@@ -29,19 +30,23 @@ export function calcDateOnDiet(
   if (weightGainKg < 0) {
     throw new Error(`This diet is for gaining weight, not loosing it!`);
   }
+  // TODO: Diesen Check in eine eigene kleine Funktion schieben
   if (ageY < 16 || heightM < 1.5) {
     throw new Error(`You do not qualify for this kind of diet.`);
   }
   let dailyCaloriesOnDiet = 0;
+  // TODO: Die Schleife schöner schreiben (ohne index)
   for (const index in foodNames) {
     const calories = foodCalories[index] || 0;
     const servings = foodServings[index] || 0;
     dailyCaloriesOnDiet += calories * servings;
   }
   let dailyCaloriesBasicMetabolicRate = 0;
+  // TODO: Die Formel für Kalorien in eine eigene Funktion auslagern
   if (sex == Sex.Male) {
     dailyCaloriesBasicMetabolicRate = Math.ceil(
       // Harris-Benedict-Formula (Male)
+      // TODO: Zahlen wie 66.47 Namen geben (Konstanten)
       66.47 + 13.7 * currentWeightKg + 5.003 * heightM * 100.0 - 6.75 * ageY,
     );
   } else {
@@ -55,5 +60,6 @@ export function calcDateOnDiet(
   if (dailyExcessCalories <= 0) {
     throw new Error("This diet is not sufficient for you to gain weight.");
   }
+  // TODO: Die Zahl 9000 als Konstante speichern
   return Math.ceil((9000 * weightGainKg) / dailyExcessCalories);
 }
