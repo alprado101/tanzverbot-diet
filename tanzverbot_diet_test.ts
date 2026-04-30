f719d002fb99ea3f9ba900d9b9d6ced1906a7e77
Alondra Prado <alondra.prado@apptiva.ch>
2026-04-30 10:42:09 +0200

    add deno test for specific man

--- tanzverbot_diet_test.ts
+++ tanzverbot_diet_test.ts
@@ -1,6 +1,14 @@
-import { assertGreater } from "@std/assert";
+import { assertGreater, assertEquals } from "@std/assert";
 import { calcDateOnDiet, Sex } from "./tanzverbot_diet.ts";
 
 Deno.test("Tanzverbot Diet", () => {
   assertGreater(calcDateOnDiet(74, 100, 1.86, 38, Sex.Male), 0.0);
 });
+
+Deno.test("Rechne Dauer aus", () => {
+  // Test: 80kg auf 85kg, 1.80m, 25 Jahre, Mann
+  const tage = calcDateOnDiet(80, 85, 1.8, 25, Sex.Male);
+  assertEquals(tage, 7);
+});
+
+
