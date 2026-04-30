b012c97f634554d5c4f8535acb846e3d7f5a73fd
Alondra Prado <alondra.prado@apptiva.ch>
2026-04-30 10:47:31 +0200

    use food objects instead of parallel arrays

--- tanzverbot_diet.ts
+++ tanzverbot_diet.ts
@@ -3,21 +3,24 @@ export enum Sex {
   Female = "f",
 }
 
-// TODO: Die drei Listen unten zu einer Liste mit Objekten machen
-const foodNames: string[] = [
-  "Kellogg's Tresor",
-  "Weihenstephan Haltbare Milch",
-  "Mühle Frikadellen",
-  "Volvic Tee",
-  "Neuburger lockerer Sahnepudding",
-  "Lagnese Viennetta",
-  "Schöller 10ForTwo",
-  "Ristorante Pizza Salame",
-  "Schweppes Ginger Ale",
-  "Mini Babybel",
+interface Food {
+  name: string;
+  calories: number;
+  servings: number;
+}
+
+const FOODS: Food[] = [
+  { name: "Kellogg's Tresor", calories: 137, servings: 4 },
+  { name: "Weihenstephan Haltbare Milch", calories: 64, servings: 8 },
+  { name: "Mühle Frikadellen", calories: 271, servings: 4 },
+  { name: "Volvic Tee", calories: 40, servings: 12 },
+  { name: "Neuburger lockerer Sahnepudding", calories: 297, servings: 1 },
+  { name: "Lagnese Viennetta", calories: 125, servings: 6 },
+  { name: "Schöller 10ForTwo", calories: 482, servings: 2 },
+  { name: "Ristorante Pizza Salame", calories: 835, servings: 2 },
+  { name: "Schweppes Ginger Ale", calories: 37, servings: 25 },
+  { name: "Mini Babybel", calories: 59, servings: 20 },
 ];
-const foodCalories: number[] = [137, 64, 271, 40, 297, 125, 482, 835, 37, 59];
-const foodServings: number[] = [4, 8, 4, 12, 1, 6, 2, 2, 25, 20];
 
 export function calcDateOnDiet(
   currentWeightKg: number,
@@ -35,11 +38,8 @@ export function calcDateOnDiet(
     throw new Error(`You do not qualify for this kind of diet.`);
   }
   let dailyCaloriesOnDiet = 0;
-  // TODO: Die Schleife schöner schreiben (ohne index)
-  for (const index in foodNames) {
-    const calories = foodCalories[index] || 0;
-    const servings = foodServings[index] || 0;
-    dailyCaloriesOnDiet += calories * servings;
+  for (const food of FOODS) {
+    dailyCaloriesOnDiet += food.calories * food.servings;
   }
   let dailyCaloriesBasicMetabolicRate = 0;
   // TODO: Die Formel für Kalorien in eine eigene Funktion auslagern
